d80c73f137aa9720791433773ed8badbf86a8f6d
angelo <aurtula1@gmail.com>
2022-01-23 03:20:43 -0500

    commit tree 52733a8032f381835461609efff9d9ea066312e5

--- src/components/LoginForm.js
+++ src/components/LoginForm.js
@@ -4,22 +4,37 @@ class LoginForm extends React.Component {
   constructor() {
     super();
 
-    this.state = {};
+    this.state = {
+      username: "",
+      password: ""
+    };
   }
+
+  handleChange = event => {
+    this.setState({
+      [event.target.name]: event.target.value
+    })
+  }
+
+  submitThisPls = event => {
+    event.preventDefault();
+    this.state.username == '' || this.state.password == "" ? null : this.props.handleLogin(this.state.username, this.state.password)
+  }
+
 
   render() {
     return (
-      <form>
+      <form onSubmit={event => this.submitThisPls(event)}>
         <div>
           <label>
             Username
-            <input id="username" name="username" type="text" />
+            <input id="username" name="username" type="text" value={this.state.username} onChange={this.handleChange}/>
           </label>
         </div>
         <div>
           <label>
             Password
-            <input id="password" name="password" type="password" />
+            <input id="password" name="password" type="password" value={this.state.password} onChange={this.handleChange}/>
           </label>
         </div>
         <div>
--- src/components/TwitterMessage.js
+++ src/components/TwitterMessage.js
@@ -4,14 +4,23 @@ class TwitterMessage extends React.Component {
   constructor() {
     super();
 
-    this.state = {};
+    this.state = {
+      message: ""
+    };
+  }
+
+  handleChange = event => {
+    this.setState({
+      message: event.target.value
+    })
   }
 
   render() {
     return (
       <div>
         <strong>Your message:</strong>
-        <input type="text" name="message" id="message" />
+        <input type="text" name="message" id="message" value={this.state.message} onChange={event => this.handleChange(event)}/>
+        <h1 name="characterCounter">{this.props.maxChars - this.state.message.length} Characters left</h1>
       </div>
     );
   }
